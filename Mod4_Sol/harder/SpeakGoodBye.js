10b2fdc17a916c108a22450cc9ade91a670f9339
Dibba <62563081+Debasree97@users.noreply.github.com>
2021-02-21 17:24:53 +0600

    Update SpeakGoodBye.js

--- Mod4_Sol/harder/SpeakGoodBye.js
+++ Mod4_Sol/harder/SpeakGoodBye.js
@@ -21,12 +21,11 @@
 // xxxx.xxxx = byeSpeaker;
 
 (function(window){
-	var helloSpeaker = {};
-	var speakWord = "Hello";
-	helloSpeaker.speak = function(names) {
+	var byeSpeaker = {};
+	var speakWord = "Good Bye";
+	byeSpeaker.speak = function(names) {
 		console.log(speakWord + " " + names);
     } 
 
-    window.helloSpeaker = helloSpeaker;
-
+    window.byeSpeaker = byeSpeaker;
 })(window);
